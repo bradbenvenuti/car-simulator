0ab55daed5813bcf48a022c184e609a82ba976ec
Brad Benvenuti <bradbenvenuti@gmail.com>
2017-05-19 11:02:18 -0400

    use generator

--- clone.py
+++ clone.py
@@ -1,59 +1,65 @@
 import csv
 import cv2
 import numpy as np
+import sklearn
 
+from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
-# Read CSV files to get driving data
+# Read CSV file to get driving data
 lines = []
-with open('../drivingdata4/driving_log.csv') as csvfile:
+with open('../drivingdata/driving_log.csv') as csvfile:
 	reader = csv.reader(csvfile)
 	for line in reader:
 		lines.append(line)
 
-# with open('../drivingdata2/driving_log.csv') as csvfile:
-# 	reader = csv.reader(csvfile)
-# 	for line in reader:
-# 		lines.append(line)
+# Split data into train / validation sets
+train_samples, validation_samples = train_test_split(lines, test_size=0.2)
 
-with open('../drivingdata3/driving_log.csv') as csvfile:
-	reader = csv.reader(csvfile)
-	for line in reader:
-		lines.append(line)
+def generator(samples, batch_size=32*3):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size/3):
+            batch_samples = samples[offset:offset+batch_size/3]
 
-images = []
-measurements = []
+			images = []
+			measurements = []
 
-# Load the images and measurements
-for line in lines:
-	steeringCorrection = 0.1
-	steeringAngles = []
-	steeringAngles.append(float(line[3]))
-	steeringAngles.append(steeringAngles[0] + steeringCorrection)
-	steeringAngles.append(steeringAngles[0] - steeringCorrection)
-	for i in range(3):
-		# get relative path for image so this works on all machines
-		source_path = line[i]
-		split = source_path.split('/')
-		filename = split[-1]
-		dir = split[-3]
-		local_path = '../' + dir + '/IMG/' + filename
-		image = cv2.imread(local_path)
-		images.append(image)
-		# create flipped version
-		images.append(np.fliplr(image))
-		# load the steering measurements
-		measurement = steeringAngles[i]
-		measurements.append(measurement)
-		# create flipped measurement
-		measurements.append(-measurement)
+			# Load the images and measurements
+			for batch_sample in batch_samples:
+				steeringCorrection = 0.1
+				steeringAngles = []
+				steeringAngles.append(float(line[3]))
+				steeringAngles.append(steeringAngles[0] + steeringCorrection)
+				steeringAngles.append(steeringAngles[0] - steeringCorrection)
+				for i in range(3):
+					# get relative path for image so this works on all machines
+					source_path = line[i]
+					filename = source_path.split('/')[-1]
+					local_path = '../drivingdata/IMG/' + filename
+					image = cv2.imread(local_path)
+					images.append(image)
+					# create flipped version
+					images.append(np.fliplr(image))
+					# load the steering measurements
+					measurement = steeringAngles[i]
+					measurements.append(measurement)
+					# create flipped measurement
+					measurements.append(-measurement)
 
-# Convert to numpy arrays
-x_train = np.array(images)
-y_train = np.array(measurements)
+			# Convert to numpy arrays
+			x_train = np.array(images)
+			y_train = np.array(measurements)
+			yield sklearn.utils.shuffle(x_train, y_train)
+
+
+# compile and train the model using the generator function
+train_generator = generator(train_samples, batch_size=32*3)
+validation_generator = generator(validation_samples, batch_size=32*3)
 
 # Create the model
 model = Sequential()
@@ -61,7 +67,7 @@ model = Sequential()
 # Normalize Data
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
 # Crop Data
-model.add(Cropping2D(cropping=((50,20), (0,0))))
+model.add(Cropping2D(cropping=((50,30), (0,0))))
 
 # Convolutions
 model.add(Convolution2D(24, 5, 5, subsample=(2,2), activation='relu'))
@@ -77,7 +83,7 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(x_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=3)
+model.fit_generator(train_generator, samples_per_epoch=len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=3)
 
 model.save('model.h5')
 
