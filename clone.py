d6578a99a94537d26e4b801de6ff6f26437274b9
Brad Benvenuti <bradbenvenuti@gmail.com>
2017-05-19 13:45:14 -0400

    add epochs

--- clone.py
+++ clone.py
@@ -66,7 +66,7 @@ model = Sequential()
 # Normalize Data
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
 # Crop Data
-model.add(Cropping2D(cropping=((50,20), (0,0))))
+model.add(Cropping2D(cropping=((60,30), (0,0))))
 
 # Convolutions
 model.add(Convolution2D(24, 5, 5, subsample=(2,2), activation='relu'))
@@ -82,7 +82,7 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit_generator(train_generator, samples_per_epoch=9024, validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=5)
+model.fit_generator(train_generator, samples_per_epoch=9024, validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=4)
 
 model.save('model.h5')
 
