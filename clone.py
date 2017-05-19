790e08583462fe5fce9f36a338fe4e7d3b3693a6
Brad Benvenuti <bradbenvenuti@gmail.com>
2017-05-19 13:47:20 -0400

    add epochs

--- clone.py
+++ clone.py
@@ -82,7 +82,7 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit_generator(train_generator, samples_per_epoch=9024, validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=4)
+model.fit_generator(train_generator, samples_per_epoch=9024, validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=3)
 
 model.save('model.h5')
 
